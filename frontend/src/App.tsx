1ffea46d0d7c896db04a48b6e1100b64d5dca34b
Thiganth-K <thiganthworkspace02@gmail.com>
2025-11-08 20:52:23 +0530

    removed the navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 
 import React, { useEffect } from 'react';
 import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -27,8 +26,7 @@ const AppContent: React.FC = () => {
 
     return (
         <div className="flex flex-col min-h-screen">
-            <Navbar />
-            <main className="flex-grow pt-20"> {/* Add padding top to avoid content being hidden by fixed navbar */}
+            <main className="flex-grow"> {/* Removed navbar and padding */}
                 <ScrollToTop />
                 <Routes>
                     <Route path="/" element={<Home />} />
